ea5a10b0e6efe5d176fee3fffa0aa78395a3cfe3
r0ckz <49621441+r0ckz@users.noreply.github.com>
2021-11-23 19:52:38 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# sftphp
-Old style FTP/HTTP directory listing in the browser, but for SFTP (using PHP)
+# SFTP Directory Browsing
+A single file PHP script to browse (external) SFTP servers like old FTP directory listings in browsers or HTTP servers without an index.
+
+This script doesn't serve files, because my goal was to be able to copy direct SFTP links for use in other apps like VLC Media Player. This means you can only see what files are in folders, but with small tweaks to the script the library used provides file streaming and serving too. You don't need full SSH access to the server and you can host it anywhere, because it works with remote servers too. But keep in mind that if the server is local-only, you need to host this script in your local network too of course.
+
+You need to download phpseclib, put this script in the phpseclib folder and serve that with a PHP-enabled webserver. This library makes it a little less "single file", but is much more efficient than using a bulky script created with plain php_ssh2 for example.
+
+Logging in can be done in multiple ways, but it happens with $\_GET by default. This way you can easily access multiple servers quickly, without the need for multiple files.
+```
+index.php?user=USERNAME&pass=PASSWORD&ip=IPADDRESS&port=PORT
+```
+
+## Dependencies:
+* [phpseclib (tested with 1.0.19)](http://phpseclib.sourceforge.net/)
+* A webserver with PHP
+
+## Screenshots
+![](./screenshot.png)
--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
 # Tested with phpseclib1.0.19, which is a dependency. Unpack it, put this script in the phpseclib folder and run it from there.
 # Other options without phpseclib include SSH2 for PHP (https://windows.php.net/downloads/pecl/releases/ssh2/ > php_ssh2-XXX-PHPVERSION-ts-x64.zip for XAMPP, put in php/ext and put or uncomment 'extension=php_ssh2.dll' in php.ini. Or 'sudo apt-get install libssh2–1-dev libssh2–1' for Debian-based systems). This is an extension rather than a library, but is much more of a hassle to setup and loop through. Especially detecting directories is why I chose phpseclib.
+# Doesn't stream or serve files, only for browsing and getting SFTP links for use in applications like VLC - which was the purpose of this script. For playing or downloading files through the browser, there are enough alternatives or you can create an extra page to 'get' files with phpseclib and serve it over http. Change the url for files in the script and you're done.
 #
 # Usage: http://localhost/index.php?user=USERNAME&pass=PASSWORD&ip=IPADDRESS&port=PORT
 # Optional: &dir= to specify start dir. Defaults to '/'.
 #
 include('Net/SFTP.php');
 
+if (!isset($_GET['ip'], $_GET['port'], $_GET['user'], $_GET['pass'])) {
+    exit('The connection settings (<i>host</i>, <i>port</i>, <i>username</i> and <i>password</i>) need to be specified or are invalid.<br>You can do that by adding the following parameters to the URL: <a href="?user=USERNAME&pass=PASSWORD&ip=IPADDRESS&port=PORT">?user=USERNAME&pass=PASSWORD&ip=IPADDRESS&port=PORT</a>'); // If required $_GET details are missing. Port will default to 22 when not or incorrectly specified, but the missing $_GET var would still cause PHP errors so instead of trying to catch that, we just require the $_GET var.
+}
+
 $sftp = new Net_SFTP($_GET['ip'] . ':' . $_GET['port']); // We specify the login details with $_GET vars. Alternatively, we could do $_SESSION vars or hardcode the ip/port/username/pass in this script (by replacing $_GET['ip'] with '127.0.0.1' for example).
 if (!$sftp->login($_GET['user'], $_GET['pass'])) {
-    exit('SFTP connection could not be established.'); // If $_GET details are wrong.
+    exit('SFTP connection could not be established.'); // If one or more of the $_GET details are wrong. Alt: 'Unable to establish an SSH connection. Is the SSH server running?'
 }
 
 if(!isset($_GET['dir'])){ $_GET['dir'] = ''; } // Set dir variable (empty) if not set, to prevent script errors. This will default to '/'.
@@ -28,14 +33,14 @@ echo '<html>
 </head>
 <body>
 <h1>Index of '.($_GET['dir'] == '' ? '/' : $_GET['dir']).'</h1>
-<pre><a href="'.$urlwithoutdir.'&dir='.str_replace('/'.basename($_GET['dir']), '', $_GET['dir']).'">[To Parent Directory]</a><br><br><table><tr><th colspan="3"><hr></th></tr><tr><th>Name</th><th>Size</th><th>Last modified</th></tr>'; // Table stuff and link to previous folder: strip the basename (current directory name) of the full $_GET['dir'] path.
+<pre>'.($_GET['dir'] == '' ? '' : '<a href="'.$urlwithoutdir.'&dir='.str_replace('/'.basename($_GET['dir']), '', $_GET['dir']).'">[To Parent Directory]</a><br><br>').'<table><tr><th colspan="3"><hr></th></tr><tr><th>Name</th><th>Size</th><th>Last modified</th></tr>'; // Table stuff and link to previous folder: strip the basename (current directory name) of the full $_GET['dir'] path.
 
 if ($sftp->is_dir($_GET['dir'])) {
 foreach($sftp->rawlist($_GET['dir']) as $filename => $file) {
     if ($file['type'] == NET_SFTP_TYPE_REGULAR) { // File? Link SFTP url, show filename, filesize and modified date.
         echo '<tr><td><a href="sftp://'.$_GET['user'].':'.$_GET['pass'].'@'.$_GET['ip'].':'.$_GET['port'].$_GET['dir'].'/'.$filename.'">' . $filename . '</a></td><td>' .$file['size'].'</td><td>'.date('d-m-Y H:i:s', $file['mtime']).'</td>';
     }
-    if ($file['type'] == NET_SFTP_TYPE_DIRECTORY && $filename !== '.' && $filename !== '..') { // Directory? Link to browse, show filename and modified date.
+    if ($file['type'] == NET_SFTP_TYPE_DIRECTORY && $filename !== '.' && $filename !== '..') { // Directory? Link to browse, show filename and modified date. Hide . and .. because these aren't real folders and we already have a backlink to the previous folder (links to . and .. don't go back).
         echo '<tr><td><a href="'.$urlwithoutdir.'&dir='.$_GET["dir"].'/'.$filename.'">' . $filename . '</a></td><td>-</td><td>'.date('d-m-Y H:i:s', $file['mtime']).'</td>';
     }
 }
